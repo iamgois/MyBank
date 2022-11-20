0ceec2024256353d7c9101e74412bddd72b0aab7
iamgois <thiagogoisjunior@gmail.com>
2022-11-20 15:25:41 -0300

    yes

--- src/components/CurrentValue.jsx
+++ src/components/CurrentValue.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import InsertValue from './InsertValue'
 
 export default function CurrentValue() {
   const value = localStorage.getItem('value')
@@ -6,7 +7,7 @@ export default function CurrentValue() {
   return (
     <div className='w-screen bg-violet-500 flex justify-center items-center flex-col p-4'>
         <p className='text-white'>Seu saldo</p>
-        <h1 className='text-5xl text-white font-extrabold'>${value}</h1>
+        <h1 className='text-5xl text-white font-extrabold'>${}</h1>
     </div>
   )
 }
--- src/components/InsertValue.jsx
+++ src/components/InsertValue.jsx
@@ -1,42 +1,36 @@
 import React, { useState } from 'react'
 
-export default function InsertValue() {
+export default function InsertValue(props) {
   
     const [value, setValue] = useState(0)
+    const [number, setNumber] = useState(0)
 
-    function addValue(){
-
-        let input = document.getElementById('input')
-        let inputFormated = parseInt(input.value)
-
-        let value = localStorage.getItem('value')
-        let valueFormated = parseInt(value)
-
-        let currentValue = valueFormated + inputFormated
-        localStorage.setItem('value', `${currentValue}`)
-        document.location.reload(true);
+    function addCash(){
+        let input = parseInt(document.getElementById('input').value)
+        return setNumber(number + input)
     }
 
-    function removeValue(){
+    /* function removeValue(){
 
-        let input = document.getElementById('input')
-        let inputFormated = parseInt(input.value)
+        let inputFormated = parseInt(document.getElementById('input').value)
+        let valueFormated = parseInt(localStorage.getItem('value'))
 
-        let value = localStorage.getItem('value')
-        let valueFormated = parseInt(value)
-
-        let currentValue = valueFormated - inputFormated
-        localStorage.setItem('value', `${currentValue}`)
-        document.location.reload(true);
-    }
+        if(valueFormated = NaN || valueFormated == null){
+            valueFormated = 0
+        }else{
+            let currentValue = valueFormated - inputFormated
+            localStorage.setItem('value', `${currentValue}`)
+            document.location.reload(true);
+        }        
+    } */
 
     return (
         <div className='w-screen h-28 flex flex-col justify-center items-center bg-violet-500'>
             <input type='number' id='input' className='bg-violet-300 p-3 w-64 rounded-xl'/>
             
             <div className='mt-4'>
-                <button className='bg-violet-700 text-white rounded-lg p-2 mr-2 font-bold ease-in-out duration-200 hover:bg-violet-900' onClick={addValue}>Add Value +</button>
-                <button  className='bg-violet-700 text-white rounded-lg p-2 ml-2 font-bold ease-in-out duration-200 hover:bg-violet-900'onClick={removeValue}>Remove Value -</button>
+                <button className='bg-violet-700 text-white rounded-lg p-2 mr-2 font-bold ease-in-out duration-200 hover:bg-violet-900' onClick={addCash}>Add Value +</button>
+                <button  className='bg-violet-700 text-white rounded-lg p-2 ml-2 font-bold ease-in-out duration-200 hover:bg-violet-900'>Remove Value -</button>
             </div>
         </div>
   )
